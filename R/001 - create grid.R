eac78eefad8ae66ac57138254908a204c8ad17c7
Jindra Lacko <jindra@jla-data.net>
2024-12-17 18:53:37 +0100

    grid s centroidy & metadaty

--- R/001 - create grid.R
+++ R/001 - create grid.R
@@ -4,6 +4,8 @@ library(RCzechia)
 library(dplyr)
 library(sf)
 
+velikost <- units::set_units(1, "km2")
+
 # obrysy Prahy - v metrickém CRS
 praha <- kraje() %>% 
    filter(KOD_CZNUTS3 == "CZ010") %>% 
@@ -11,9 +13,9 @@ praha <- kraje() %>%
 
 # equal area grid - kilometřík je dobrá míra...
 grid <- praha %>% 
-   st_make_grid(cellsize = units::set_units(1, "km2")) %>% 
+   st_make_grid(cellsize = velikost) %>% 
    st_as_sf() %>% 
-   mutate(id = 1:n())
+   mutate(grid_id = 1:n())
 
 # 
 
@@ -25,12 +27,46 @@ grid$vyska <- exactextractr::exact_extract(
 ) 
 
 # vzdálenost rohů gridu = dimenze matice buněk
-grid %>%
+vzdalenosti <- grid %>%
    summarise() %>%
    st_simplify() %>%
    st_cast("POINT") %>%
    unique() %>%
-   st_distance()
+   st_distance() %>% 
+   .[1, c(2, 4)] %>%  
+   as.data.frame() %>% 
+   rename(pocet = '.') %>% 
+   mutate(code = c("sirka", "delka"),
+          value = units::drop_units(pocet / sqrt(velikost))) %>% 
+   select(code, value) %>% 
+   bind_rows(data.frame(code = "roztec", value = units::drop_units(units::set_units(sqrt(velikost), "m"))))
 
-# uložit pro budoucí použití...
-st_write(grid, "./data/grid.gpkg")
+# středy čtverečků
+centroids <- grid %>% 
+   st_centroid()
+
+# uložit grid
+st_write(grid, "./data/grid.gpkg", 
+         layer = "ctverecky", 
+         append = FALSE,
+         fid_column_name = "grid_id")
+
+# uložit středy
+st_write(centroids, "./data/grid.gpkg", 
+         layer = "centroidy", 
+         append = FALSE,
+         fid_column_name = "grid_id")
+
+
+# zapsat rozměry
+library(DBI)
+
+con <- DBI::dbConnect(RSQLite::SQLite(), "./data/grid.gpkg") # připojit databázi
+
+# zahodit co bylo...
+dbExecute(con, "drop table if exists metadata;")
+# uložit do databáze
+DBI::dbWriteTable(con, "metadata", vzdalenosti)
+
+DBI::dbDisconnect(con)
+
